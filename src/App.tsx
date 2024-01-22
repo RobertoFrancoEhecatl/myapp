0b0c090d65c1bf60f06ec7629b6540f8c9de701c
robertofrancotenorio2 <robertofrancotenorio2@gmail.com>
2024-01-22 17:28:40 +0000

    Axion Agregado correctamente

--- src/App.tsx
+++ src/App.tsx
@@ -14,106 +14,108 @@ function App() {
   /* Porque se recomienda el uso del arreglo vacío en useEffect? */
   useEffect(() => {
     try{
-      fetch("https://dog.ceo/api/breeds/image/random")
-      .then((response) => response.json())
-      .then((dog) => {
-        setImageUrl(dog.message); // ⬅️ Guardar datos
-        setIsLoading(false); // ⬅️ Desactivar modo "cargando"
-      });
+      axios.get("https://dog.ceo/api/breeds/image/random")
+      .then((response) => {
+        console.log("response", response)
+        setImageUrl(response.data.message);
+      })
+      .finally(() => {
+        setIsLoading(false)
+      })
     }
     catch(e){
-      
+      console.log("error", e)
     }
-  },);
+  }, []);
 
-    
-    if (isLoading) { // ⬅️ si está cargando, mostramos un texto que lo indique
-      return (
-        <div className="App">
-          <h1>Cargando...</h1>
-        </div>
-      );
-    }
 
+  if (isLoading) { // ⬅️ si está cargando, mostramos un texto que lo indique
     return (
       <div className="App">
-        <Moveable target={document.querySelector(".image")}
-          container={null}
-          origin={true}
+        <h1>Cargando...</h1>
+      </div>
+    );
+  }
 
-          /* Resize event edges */
-          edge={false}
+  return (
+    <div className="App">
+      <Moveable target={document.querySelector(".image")}
+        container={null}
+        origin={true}
 
-          /* draggable */
-          draggable={true}
-          throttleDrag={0}
-          onDragStart={({ target, clientX, clientY }) => {
-              console.log("onDragStart", target);
-          }}
-          onDrag={({
-              target,
-              beforeDelta, beforeDist,
-              left, top,
-              right, bottom,
-              delta, dist,
-              transform,
-              clientX, clientY,
-          }: OnDrag) => {
-              console.log("onDrag left, top", left, top);
-              // target!.style.left = `${left}px`;
-              // target!.style.top = `${top}px`;
-              console.log("onDrag translate", dist);
-              target!.style.transform = transform;
-          }}
-          onDragEnd={({ target, isDrag, clientX, clientY }) => {
-              console.log("onDragEnd", target, isDrag);
-          }}
+        /* Resize event edges */
+        edge={false}
 
-          /* When resize or scale, keeps a ratio of the width, height. */
-          keepRatio={true}
+        /* draggable */
+        draggable={true}
+        throttleDrag={0}
+        onDragStart={({ target, clientX, clientY }) => {
+          console.log("onDragStart", target);
+        }}
+        onDrag={({
+          target,
+          beforeDelta, beforeDist,
+          left, top,
+          right, bottom,
+          delta, dist,
+          transform,
+          clientX, clientY,
+        }: OnDrag) => {
+          console.log("onDrag left, top", left, top);
+          // target!.style.left = `${left}px`;
+          // target!.style.top = `${top}px`;
+          console.log("onDrag translate", dist);
+          target!.style.transform = transform;
+        }}
+        onDragEnd={({ target, isDrag, clientX, clientY }) => {
+          console.log("onDragEnd", target, isDrag);
+        }}
 
-          /* resizable*/
-          /* Only one of resizable, scalable, warpable can be used. */
-          resizable={true}
-          throttleResize={0}
-          onResizeStart={({ target , clientX, clientY}) => {
-              console.log("onResizeStart", target);
-          }}
-          onResize={({
-              target, width, height,
-              dist, delta, direction,
-              clientX, clientY,
-          }: OnResize) => {
-              console.log("onResize", target);
-              delta[0] && (target!.style.width = `${width}px`);
-              delta[1] && (target!.style.height = `${height}px`);
-          }}
-          onResizeEnd={({ target, isDrag, clientX, clientY }) => {
-              console.log("onResizeEnd", target, isDrag);
-          }}
+        /* When resize or scale, keeps a ratio of the width, height. */
+        keepRatio={true}
 
-          /* scalable */
-          /* Only one of resizable, scalable, warpable can be used. */
-          scalable={true}
-          throttleScale={0}
-          onScaleStart={({ target, clientX, clientY }) => {
-              console.log("onScaleStart", target);
-          }}
-          onScale={({
-              target, scale, dist, delta, transform,
-              clientX, clientY,
-          }: OnScale) => {
-              console.log("onScale scale", scale);
-              target!.style.transform = transform;
-          }}
-          onScaleEnd={({ target, isDrag, clientX, clientY }) => {
-              console.log("onScaleEnd", target, isDrag);
-          }}
-        />
-        <img className='image' src={imageUrl} alt="Imagen de perrito aleatoria" />
+        /* resizable*/
+        /* Only one of resizable, scalable, warpable can be used. */
+        resizable={true}
+        throttleResize={0}
+        onResizeStart={({ target, clientX, clientY }) => {
+          console.log("onResizeStart", target);
+        }}
+        onResize={({
+          target, width, height,
+          dist, delta, direction,
+          clientX, clientY,
+        }: OnResize) => {
+          console.log("onResize", target);
+          delta[0] && (target!.style.width = `${width}px`);
+          delta[1] && (target!.style.height = `${height}px`);
+        }}
+        onResizeEnd={({ target, isDrag, clientX, clientY }) => {
+          console.log("onResizeEnd", target, isDrag);
+        }}
+
+        /* scalable */
+        /* Only one of resizable, scalable, warpable can be used. */
+        scalable={true}
+        throttleScale={0}
+        onScaleStart={({ target, clientX, clientY }) => {
+          console.log("onScaleStart", target);
+        }}
+        onScale={({
+          target, scale, dist, delta, transform,
+          clientX, clientY,
+        }: OnScale) => {
+          console.log("onScale scale", scale);
+          target!.style.transform = transform;
+        }}
+        onScaleEnd={({ target, isDrag, clientX, clientY }) => {
+          console.log("onScaleEnd", target, isDrag);
+        }}
+      />
+      <img className='image' src={imageUrl} alt="Imagen de perrito aleatoria" />
 
     </div>
-    );
+  );
 
 }
 
